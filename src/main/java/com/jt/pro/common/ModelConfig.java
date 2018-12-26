49fa4b86c360464972fad26f48e8cc070f18e550
bb <bbz525@163.com>
2018-12-26 13:37:26 +0800

    2018_12_26 ModelConfig修改的项目

--- src/main/java/com/jt/pro/common/ModelConfig.java
+++ src/main/java/com/jt/pro/common/ModelConfig.java
@@ -2,6 +2,9 @@ package com.jt.pro.common;
 
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * comment:
+ */
 @ComponentScan(basePackages = {"com.jt.pro"})
 public class ModelConfig {
 }
